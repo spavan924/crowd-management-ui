849fc14cd4765f1174f0dac0ee809b3c022fbfcf
Sai Pavan <spavan924@gmail.com>
2025-12-17 21:42:58 +0530

    Fix Angular SSR handler for Vercel

--- api/server.ts
+++ api/server.ts
@@ -1,3 +1,5 @@
-import app from '../dist/crowd-management-ui/server/server.mjs';
+import server from '../dist/crowd-management-ui/server/server.mjs';
 
-export default app;
+export default function handler(req: any, res: any) {
+  return server(req, res);
+}
